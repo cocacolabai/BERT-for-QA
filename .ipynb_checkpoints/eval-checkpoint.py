a326ad98f9242f91cae0a5ebc9918794c7bb18d6
weibig <wendydaan@gmail.com>
2020-04-25 19:28:29 +0000

    eval revise

--- .ipynb_checkpoints/eval-checkpoint.py
+++ .ipynb_checkpoints/eval-checkpoint.py
@@ -661,8 +661,8 @@ with torch.no_grad():
 #       writer.write(json.dumps(scores_diff_json, indent=4) + "\n")
 
 
-def get_final_text(pred_text, orig_text, do_lower_case):
-  """Project the tokenized prediction back to the original text."""
+# def get_final_text(pred_text, orig_text, do_lower_case):
+#   """Project the tokenized prediction back to the original text."""
 
   # When we created the data, we kept track of the alignment between original
   # (whitespace tokenized) tokens and our WordPiece tokenized tokens. So
@@ -689,71 +689,71 @@ def get_final_text(pred_text, orig_text, do_lower_case):
   # `pred_text` and `orig_text` to get a character-to-charcter alignment. This
   # can fail in certain cases in which case we just return `orig_text`.
 
-  def _strip_spaces(text):
-    ns_chars = []
-    ns_to_s_map = collections.OrderedDict()
-    for (i, c) in enumerate(text):
-      if c == " ":
-        continue
-      ns_to_s_map[len(ns_chars)] = i
-      ns_chars.append(c)
-    ns_text = "".join(ns_chars)
-    return (ns_text, ns_to_s_map)
+#   def _strip_spaces(text):
+#     ns_chars = []
+#     ns_to_s_map = collections.OrderedDict()
+#     for (i, c) in enumerate(text):
+#       if c == " ":
+#         continue
+#       ns_to_s_map[len(ns_chars)] = i
+#       ns_chars.append(c)
+#     ns_text = "".join(ns_chars)
+#     return (ns_text, ns_to_s_map)
 
   # We first tokenize `orig_text`, strip whitespace from the result
   # and `pred_text`, and check if they are the same length. If they are
   # NOT the same length, the heuristic has failed. If they are the same
   # length, we assume the characters are one-to-one aligned.
 
-  tok_text = " ".join(tokenizer.tokenize(orig_text))
+#   tok_text = " ".join(tokenizer.tokenize(orig_text))
 
-  start_position = tok_text.find(pred_text)
-  if start_position == -1:
-    if  verbose_logging:
-      tf.compat.v1.logging.info(
-          "Unable to find text: '%s' in '%s'" % (pred_text, orig_text))
-    return orig_text
-  end_position = start_position + len(pred_text) - 1
+#   start_position = tok_text.find(pred_text)
+#   if start_position == -1:
+#     if  verbose_logging:
+#       tf.compat.v1.logging.info(
+#           "Unable to find text: '%s' in '%s'" % (pred_text, orig_text))
+#     return orig_text
+#   end_position = start_position + len(pred_text) - 1
 
-  (orig_ns_text, orig_ns_to_s_map) = _strip_spaces(orig_text)
-  (tok_ns_text, tok_ns_to_s_map) = _strip_spaces(tok_text)
+#   (orig_ns_text, orig_ns_to_s_map) = _strip_spaces(orig_text)
+#   (tok_ns_text, tok_ns_to_s_map) = _strip_spaces(tok_text)
 
-  if len(orig_ns_text) != len(tok_ns_text):
-    if  verbose_logging:
-      tf.compat.v1.logging.info("Length not equal after stripping spaces: '%s' vs '%s'",
-                      orig_ns_text, tok_ns_text)
-    return orig_text
+#   if len(orig_ns_text) != len(tok_ns_text):
+#     if  verbose_logging:
+#       tf.compat.v1.logging.info("Length not equal after stripping spaces: '%s' vs '%s'",
+#                       orig_ns_text, tok_ns_text)
+#     return orig_text
 
-  # We then project the characters in `pred_text` back to `orig_text` using
-  # the character-to-character alignment.
-  tok_s_to_ns_map = {}
-  for (i, tok_index) in six.iteritems(tok_ns_to_s_map):
-    tok_s_to_ns_map[tok_index] = i
+#   # We then project the characters in `pred_text` back to `orig_text` using
+#   # the character-to-character alignment.
+#   tok_s_to_ns_map = {}
+#   for (i, tok_index) in six.iteritems(tok_ns_to_s_map):
+#     tok_s_to_ns_map[tok_index] = i
 
-  orig_start_position = None
-  if start_position in tok_s_to_ns_map:
-    ns_start_position = tok_s_to_ns_map[start_position]
-    if ns_start_position in orig_ns_to_s_map:
-      orig_start_position = orig_ns_to_s_map[ns_start_position]
+#   orig_start_position = None
+#   if start_position in tok_s_to_ns_map:
+#     ns_start_position = tok_s_to_ns_map[start_position]
+#     if ns_start_position in orig_ns_to_s_map:
+#       orig_start_position = orig_ns_to_s_map[ns_start_position]
 
-  if orig_start_position is None:
-    if  verbose_logging:
-      tf.compat.v1.logging.info("Couldn't map start position")
-    return orig_text
+#   if orig_start_position is None:
+#     if  verbose_logging:
+#       tf.compat.v1.logging.info("Couldn't map start position")
+#     return orig_text
 
-  orig_end_position = None
-  if end_position in tok_s_to_ns_map:
-    ns_end_position = tok_s_to_ns_map[end_position]
-    if ns_end_position in orig_ns_to_s_map:
-      orig_end_position = orig_ns_to_s_map[ns_end_position]
+#   orig_end_position = None
+#   if end_position in tok_s_to_ns_map:
+#     ns_end_position = tok_s_to_ns_map[end_position]
+#     if ns_end_position in orig_ns_to_s_map:
+#       orig_end_position = orig_ns_to_s_map[ns_end_position]
 
-  if orig_end_position is None:
-    if  verbose_logging:
-      tf.compat.v1.logging.info("Couldn't map end position")
-    return orig_text
+#   if orig_end_position is None:
+#     if  verbose_logging:
+#       tf.compat.v1.logging.info("Couldn't map end position")
+#     return orig_text
 
-  output_text = orig_text[orig_start_position:(orig_end_position + 1)]
-  return output_text
+#   output_text = orig_text[orig_start_position:(orig_end_position + 1)]
+#   return output_text
 
 
 # def _get_best_indexes(logits, n_best_size):
